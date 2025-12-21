98b3a634ed08977b03a1a2b150aeacf2eafc312c
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2025-12-21 17:54:08 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,8 @@ const ASSETS_TO_CACHE = [
   './app.js',
   './favicon.png',
   './icon.png',
-  './manifest.json'
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
 ];
 
 // Install Event: Cache core assets
