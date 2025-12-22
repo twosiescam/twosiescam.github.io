e01a9f6bb15ccedf1df98802794578d4be8cbef0
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2025-12-22 10:18:32 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ const ASSETS_TO_CACHE = [
   './favicon.png',
   './icon.png',
   './manifest.json',
+  './font/vcr.ttf', 
   'https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
 ];
 
